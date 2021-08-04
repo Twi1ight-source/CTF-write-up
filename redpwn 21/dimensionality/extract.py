188d1e1957c734890ec437442137db3d7fc1709b
Twi1ight-source <84214843+Twi1ight-source@users.noreply.github.com>
2021-08-04 08:39:06 +0700

    Update extract.py

--- redpwn 21/dimensionality/extract.py
+++ redpwn 21/dimensionality/extract.py
@@ -1,2 +1,12 @@
-a="frrffllffddllffrrffuubbrrfff"
-print(len(a))
+f=open('dump','rb').read()
+
+arr=[]
+end=f.find(b'\x03')
+f=f[:end+1]
+arr_1=[]
+for i in range(len(f)):
+    arr.append(f[i])
+for i in range(len(arr)):
+    if arr[i]!=0:
+        arr_1.append(i)
+print(arr_1)
