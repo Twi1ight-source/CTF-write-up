9c156bb1506b9c19eb779e26c130cb45a99fed34
Twi1ight-source <84214843+Twi1ight-source@users.noreply.github.com>
2021-09-16 15:45:10 +0700

    Update diass.py

--- RarCTF/2021/very boring checker/diass.py
+++ RarCTF/2021/very boring checker/diass.py
@@ -1,17 +1,10 @@
-from ctypes import *
 
 def check(a1):
     ip,v11=0,0
     mem_ptr, stack_ptr, inp_ptr =0,0,0
-    stack = []
-    for i in range(256):
-        stack.append(c_uint8(0))
-
-    mem = []
-    for i in range(256):
-        mem.append(c_int(0))
+    stack = [0]*256
+    mem = [0]*256
     
-
     while ip < len(code):
         a=(code[ip]>>7)>>5
         ins=(a+ code[ip])&7 -a 
@@ -23,13 +16,13 @@ def check(a1):
                 
 
             elif ins==1:
-                print(f'{ip, ins}: if (stack[{stack_ptr}]({stack[stack_ptr].value})): ip=mem[mem_ptr-1](ip={mem[mem_ptr-1].value})')
-                print(f'           else: mem[mem_ptr-1]=0({mem[mem_ptr-1].value}), mem_ptr--({mem_ptr}--)')
-                if (stack[stack_ptr].value !=0):
-                    ip=mem[mem_ptr-1].value
+                print(f'{ip, ins}: if (stack[{stack_ptr}]({stack[stack_ptr]})): ip=mem[mem_ptr-1](ip={mem[mem_ptr-1]})')
+                print(f'           else: mem[mem_ptr-1]=0({mem[mem_ptr-1]}), mem_ptr--({mem_ptr}--)')
+                if (stack[stack_ptr] !=0):
+                    ip=mem[mem_ptr-1]
                 else:
                     mem_ptr-=1
-                    mem[mem_ptr].value=0
+                    mem[mem_ptr]=0
                 
             
             elif ins==2:
@@ -38,10 +31,10 @@ def check(a1):
                 
             
             elif ins==3:
-                print(f'{ip, ins}: if stack[{stack_ptr}] ({stack[stack_ptr].value}): mem[mem_ptr]=ip (={ip}), mem_ptr+=1 ({mem_ptr}+=1)')
+                print(f'{ip, ins}: if stack[{stack_ptr}] ({stack[stack_ptr]}): mem[mem_ptr]=ip (={ip}), mem_ptr+=1 ({mem_ptr}+=1)')
                 print(f'              else: v11+=1 ({v11}+=1)')
-                if (stack[stack_ptr].value!=0):
-                    mem[mem_ptr].value=ip
+                if (stack[stack_ptr]!=0):
+                    mem[mem_ptr]=ip
                     mem_ptr+=1
                     
                 else:
@@ -53,21 +46,26 @@ def check(a1):
                 
                 if not inp_ptr:
                     inp= a1+ '\n'.ljust(64, '\x00')
-                stack[stack_ptr].value=ord(inp[inp_ptr])
+                stack[stack_ptr]=ord(inp[inp_ptr])
                 print(f'stack[{stack_ptr}]= inp[inp_ptr](={ord(inp[inp_ptr])})')
                 inp_ptr+=1
                     
             
             elif ins==5:
-                print(f'{ip, ins}: putchar({chr(stack[stack_ptr].value)})')
+                print(f'{ip, ins}: putchar({chr(stack[stack_ptr])})')
                 
             elif ins==6:
-                print(f'{ip, ins}: stack[{stack_ptr}]-=1 ({stack[stack_ptr].value}-=1)')
-                stack[stack_ptr].value-=1
+                if stack[stack_ptr]<0:
+                    stack[stack_ptr]=255        
+                print(f'{ip, ins}: stack[{stack_ptr}]-=1 ({stack[stack_ptr]}-=1)')
+                stack[stack_ptr]-=1
                 
+                   
             elif ins==7:
-                print(f'{ip, ins}: stack[{stack_ptr}]+=1 ({stack[stack_ptr].value}+=1)')
-                stack[stack_ptr].value+=1
+                if stack[stack_ptr] ==255:
+                    stack[stack_ptr]=-1
+                print(f'{ip, ins}: stack[{stack_ptr}]+=1 ({stack[stack_ptr]}+=1)')
+                stack[stack_ptr]+=1
                
             else:
                 break
